9f21722d86d4fe7c2175fa84e64c9cb190d616f1
Harold Thetiot <hthetiot@gmail.com>
2024-04-23 17:53:54 +0200

    fix PHP typos into formulaire_HMAC.php and formulaire_HMAC_iFrame.php with proper indentation, , ,  for recette-tpeweb

--- formulaire_HMAC_iFrame.php
+++ formulaire_HMAC_iFrame.php
@@ -11,16 +11,22 @@ function Formatage($value, $maxLength) {
 
 // --------------- DÉCLARATION DES VARIABLES ---------------
 
-$pbx_site = 'votre n° de site';															//Numéro de site
-$pbx_rang = 'votre n° de rang';															//Numéro de rang
-$pbx_identifiant = 'votre n° d identifiant site';										//Identifiant de site
-$pbx_total = 'votre montant';															//Montant de la commande
+
+// Renseignez information récupéré depuis le back-office Vision à cet endroit
+$pbx_site = '1999887';		// Numéro de site
+$pbx_rang = '32';		// Numéro de rang
+$pbx_identifiant = '215';	// Identifiant de site
+$pbx_total = '199';		// Montant de la commande
+
 // Suppression des points ou virgules dans le montant						
 $pbx_total = str_replace(",", "", $pbx_total);
 $pbx_total = str_replace(".", "", $pbx_total);
 
-$pbx_cmd = 'votre n° de commande';														//Numéro de commande
-$pbx_porteur = 'email de l acheteur';													//Email de l'acheteur
+$pbx_cmd = '123';			// Numéro de commande		
+$pbx_porteur = 'test@example.com';	// Email de l'acheteur
+
+// Nombre de produit envoyé dans PBX_SHOPPINGCART :
+$pbx_nb_produit = '1';	// Nombre de produits dans le panier
 
 // Paramétrage de l'url de retour back office site (notification de paiement IPN) :
 $pbx_repondre_a = 'https://www.votre-site.extention/page-de-back-office-site';
@@ -36,26 +42,29 @@ $pbx_refuse = 'https://www.votre-site.extention/refuse.php';
 // On récupère la date au format ISO-8601 :
 $dateTime = date("c");
 
-// Nombre de produit envoyé dans PBX_SHOPPINGCART :
-$pbx_nb_produit = 'nombre de produit dans le panier';									//Nombre de produits dans le panier
+// ---------------  ---------------
+
 // Construction de PBX_SHOPPINGCART :
 $pbx_shoppingcart = "<?xml version=\"1.0\" encoding=\"utf-8\"?><shoppingcart><total><totalQuantity>".$pbx_nb_produit."</totalQuantity></total></shoppingcart>";
+
 // Choix de l'authentification dans PBX_SOUHAITAUTHENT
-$pbx_souhaitauthent = '02';		//Variable de souhait authentification 3DS (01 par défaut, 02 pour exemption 3DS)
+$pbx_souhaitauthent = '02';	// Variable de souhait authentification 3DS (01 par défaut, 02 pour exemption 3DS)
+
+// Vérification du montant maximal pour l'exemption 3DS
 if($pbx_total > 3000) {
-	$pbx_souhaitauthent = '01';	// Vérification du montant maximal pour l'exemption 3DS
-}}
+	$pbx_souhaitauthent = '01';	
+}
 
 // Valeurs envoyées dans PBX_BILLING :
-$pbx_prenom_fact = Formatage('prenom de l utilisateur de facturation', 22);		//Variable prénom du porteur
-$pbx_nom_fact = Formatage('nom de l utilisateur de facturation', 22);			//Variable nom du porteur
-$pbx_adresse1_fact = Formatage('ligne1 de l adresse de facturation', 50);		//Variable adresse ligne 1 du porteur
-$pbx_adresse2_fact = Formatage('ligne2 de l adresse de facturation', 50);		//Variable adresse ligne 2 du porteur
-$pbx_zipcode_fact = Formatage('code postal de l adresse de facturation', 16);	//Variable code postal du porteur
-$pbx_city_fact = Formatage('ville de l adresse de facturation', 50);			//Variable ville du porteur
-$pbx_country_fact = Formatage('code pays iso-3166-1 numérique de l adresse de facturation', 3);		//Variable pays du porteur
-$pbx_country_code_mobile_phone = '+33'											//Variable indicatif pays du numero de telephone mobile du porteur
-$pbx_mobile_phone = '0612345675'												//Variable numero de telephone mobile du porteur
+$pbx_prenom_fact = Formatage('prenom de l utilisateur de facturation', 22);	// Variable prénom du porteur
+$pbx_nom_fact = Formatage('nom de l utilisateur de facturation', 22);		// Variable nom du porteur
+$pbx_adresse1_fact = Formatage('ligne1 de l adresse de facturation', 50);	// Variable adresse ligne 1 du porteur
+$pbx_adresse2_fact = Formatage('ligne2 de l adresse de facturation', 50);	// Variable adresse ligne 2 du porteur
+$pbx_zipcode_fact = Formatage('code postal de l adresse de facturation', 16);	// Variable code postal du porteur
+$pbx_city_fact = Formatage('ville de l adresse de facturation', 50);		// Variable ville du porteur
+$pbx_country_fact = Formatage('code pays iso-3166-1 numérique de l adresse de facturation', 3);	// Variable pays du porteur
+$pbx_country_code_mobile_phone = '+33';	// Variable indicatif pays du numero de telephone mobile du porteur
+$pbx_mobile_phone = '0612345675';	// Variable numero de telephone mobile du porteur
 
 // Construction de PBX_BILLING :
 $pbx_billing = "<?xml version=\"1.0\" encoding=\"utf-8\"?><Billing><Address><FirstName>".$pbx_prenom_fact."</FirstName>".
@@ -68,34 +77,43 @@ $pbx_billing = "<?xml version=\"1.0\" encoding=\"utf-8\"?><Billing><Address><Fir
 
 // --------------- TESTS DE DISPONIBILITE DES SERVEURS ---------------
 
-$serveurs = array('tpeweb.e-transactions.fr', //serveur primaire
-'tpeweb1.e-transactions.fr'); //serveur secondaire
+// Activation de l'univers de production
+$serveurs = array(
+	'tpeweb.e-transactions.fr', // principale
+	'tpeweb1.e-transactions.fr' // secondaire
+);
+
+// Activation de l'univers de recette
+$serveurs = array('recette-tpeweb.e-transactions.fr');
+
 $serveurOK = "";
 
 foreach($serveurs as $serveur){
 	$doc = new DOMDocument();
 	$doc->loadHTMLFile('https://'.$serveur.'/load.html');
 	$server_status = "";
+
 	$element = $doc->getElementById('server_status');
-	if($element){
-	$server_status = $element->textContent;}
+	if($element) {
+		$server_status = $element->textContent;
+	}
+
 	if($server_status == "OK"){
 		// Le serveur est prêt et les services opérationnels
 		$serveurOK = $serveur;
-	break;}
+		break;
+	}
 	// else : La machine est disponible mais les services ne le sont pas.
 }
-//curl_close($ch);
-if(!$serveurOK){
-die("Erreur : Aucun serveur n'a été trouvé");}
-// Activation de l'univers de recette
-//$serveurOK = 'recette-tpeweb.e-transactions.fr';
+
+if(!$serveurOK) {
+	die("Erreur : Aucun serveur n'a été trouvé");
+}
 
 //Création de l'url e-Transactions
 $urletrans = 'https://'.$serveurOK.'/php/';
 echo "Serveur ".$serveurOK;
 echo "<br><br>";
-
 
 // --------------- TRAITEMENT DES VARIABLES ---------------
 
@@ -118,9 +136,11 @@ $msg = "PBX_SITE=".$pbx_site.
 "&PBX_BILLING=".$pbx_billing.
 "&PBX_SOUHAITAUTHENT=".$pbx_souhaitauthent;
 
-
 // --------------- RÉCUPÉRATION ET FORMATAGE DE LA CLÉ HMAC ---------------
-$hmac = '0123456789ABCDEF0123456789ABCDEF0123456789ABCDEF0123456789ABCDEF0123456789ABCDEF0123456789ABCDEF0123456789ABCDEF0123456789ABCDEF'; //Renseignez votre clé HMAC récupéré depuis le back-office Vision à cet endroit
+
+//Renseignez votre clé HMAC récupéré depuis le back-office Vision à cet endroit
+$hmac = '0123456789ABCDEF0123456789ABCDEF0123456789ABCDEF0123456789ABCDEF0123456789ABCDEF0123456789ABCDEF0123456789ABCDEF0123456789ABCDEF'; 
+
 $pbx_hmac = strtoupper(hash_hmac('sha512', $msg, hex2bin($hmac)));
 
 // La chaîne sera envoyée en majuscule, d'où l'utilisation de strtoupper()
@@ -149,7 +169,7 @@ $pbx_hmac = strtoupper(hash_hmac('sha512', $msg, hex2bin($hmac)));
 <input type="hidden" name="PBX_SHOPPINGCART" value="<?php echo htmlspecialchars($pbx_shoppingcart); ?>">
 <input type="hidden" name="PBX_BILLING" value="<?php echo htmlspecialchars($pbx_billing); ?>">
 <input type="hidden" name="PBX_SOUHAITAUTHENT" value="<?php echo $pbx_souhaitauthent; ?>">
-<input type="hidden" name="PBX_HMAC" value="<?php echo $hmac; ?>">
+<input type="hidden" name="PBX_HMAC" value="<?php echo $pbx_hmac; ?>">
 <input type="submit" value="Envoyer dans l'iFrame">
 </form>
 <br/><br/>
